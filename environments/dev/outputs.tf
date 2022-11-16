1c5c1e13192802454c58b82706ea182ce17ff1e1
gkharsikar <girish.kharsikar@telus.com>
2022-11-15 22:00:50 -0500

    Update project IDs and buckets

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -20,11 +20,11 @@ output "network" {
 output "subnet" {
   value = "${module.vpc.subnet}"
 }
-
+/*
 output "firewall_rule" {
   value = "${module.firewall.firewall_rule}"
 }
-
+*/
 output "instance_name" {
   value = "${module.http_server.instance_name}"
 }
